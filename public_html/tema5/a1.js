ff0da90fb87014cd5382e9b673d632ed72ea26b4
Vespertino <Vespertino@C6PC11>
2016-10-31 18:05:25 +0100

    1

---
+++ public_html/tema5/a1.js
@@ -0,0 +1,6 @@
+document.write("Vocal con tilde - Codificacion en Unicode<br><br>");
+document.write("á - " + escape("á") + "<br>");
+document.write("é - " + escape("é") + "<br>");
+document.write("í - " + escape("í") + "<br>");
+document.write("ó - " + escape("ó") + "<br>");
+document.write("ú - " + escape("ú") + "<br>");
