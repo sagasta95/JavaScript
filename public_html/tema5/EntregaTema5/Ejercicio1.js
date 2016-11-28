800e291d54c10ce23d775ab8bbb9f7893c138407
Vespertino <Vespertino@C6PC11>
2016-11-28 19:31:08 +0100

    1

--- public_html/tema5/EntregaTema5/Ejercicio1.js
+++ public_html/tema5/EntregaTema5/Ejercicio1.js
@@ -1,7 +1,23 @@
 function Club() {
     var socios = new Array();
-    nuevoSocio = new Socio()
+
+    function darAlta(cliente) {
+        this.socios.push(cliente);
+    }
+
+    function darBaja(cliente) {
+        this.socios.splice(this.socios.indexOf(cliente), 1);
+    }
+
+    function mostrarSocios() {
+        document.write("<table>");
+        for(var i = 0; i < this.socios.length; i++){
+            document.write("<tr><td>" + this.socios[i].numSocio + "</td><td>" + this.socios[i].dni + "</td><td>" + this.socios[i].nombre + "</td><td>" + this.socios[i].apellidos + "</td><td>" + this.socios[i].fecNacimiento + "</td><td>" + this.socios[i].localidad + "</td><td>" + this.socios[i].getCategoria());
+        }
+        document.write("</table>");
+    }
 }
+
 function Socio(numSocio, dni, nombre, apellidos, fecNacimiento, localidad) {
     this.numSocio = numSocio;
     this.dni = dni;
@@ -9,30 +25,15 @@ function Socio(numSocio, dni, nombre, apellidos, fecNacimiento, localidad) {
     this.apellidos = apellidos;
     this.fecNacimiento = fecNacimiento;
     this.localidad = localidad;
-}
 
-function darAlta() {
     
-}
-
-function darBaja() {
-
-}
-
-function setLocalidad(licalidad) {
-    this.localidad = localidad;
-}
-
-function mostrarSocios() {
+    function setLocalidad(localidad) {
+        this.localidad = localidad;
+    }
     
+    function getCategoria(){
+        if(this.localidad){
+        }
+    }
 }
-
-function buscarSocioDni() {
-    
-}
-
-function buscarSocioNombre() {
-    
-}
-
 
